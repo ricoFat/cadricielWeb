d0b9e936936d33ed69c443070a2ffb25665da604
ricoFat <accram2005@hotmail.com>
2023-05-12 10:17:00 -0400

    creation de la base dees donnes et controller

---
+++ my-blog/database/factories/BlogPostFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\BlogPost;
+use App\Models\User;
+
+class BlogPostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title'=>$this->faker->sentence,
+            'body'=>$this->faker->paragraph(30),
+            'user_id'=>User::factory()
+        ];
+    }
+}
--- my-blog/database/migrations/2023_05_12_130235_create_blog_posts_table.php
+++ my-blog/database/migrations/2023_05_12_130235_create_blog_posts_table.php
@@ -15,6 +15,9 @@ class CreateBlogPostsTable extends Migration
     {
         Schema::create('blog_posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title',100);
+            $table->text('body');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
