c4e3fa55bd019eef7195a25e5611d5e3be2572f0
ricoFat <accram2005@hotmail.com>
2023-05-31 23:07:40 -0400

    ajout de la documentation

--- Maisonneuve2295546/resources/views/layouts/app.blade.php
+++ Maisonneuve2295546/resources/views/layouts/app.blade.php
@@ -5,7 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-   
+    <style>
+        .dropdown-menu .dropdown-item:hover {
+          background-color: black;
+          color: white;
+        }
+      </style>
     <title> @yield('title')</title>
 </head>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
